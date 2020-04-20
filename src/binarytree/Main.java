05719701ec999cfa662adb8fcb595baa79332a56
FireflySK <mada11@azet.sk>
2020-04-20 14:26:29 +0200

    Initial commit

---
+++ src/binarytree/Main.java
@@ -0,0 +1,13 @@
+package binarytree;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Tree tree =new Tree(new Node(20,"HTTP protocol"));
+        Node node1= new Node(1,"NOdeJS");
+
+    }
+
+}
---
+++ src/binarytree/Tree.java
@@ -0,0 +1,21 @@
+package binarytree;
+
+public class Tree {
+    private Node root;
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+
+    public void addNode(Node node){
+        if(node==null)
+            return;
+        root.addChild(node);
+    }
+
+    public void printInOrder()
+    {
+        if(root!=null)
+            root.inorder();
+    }
+}
