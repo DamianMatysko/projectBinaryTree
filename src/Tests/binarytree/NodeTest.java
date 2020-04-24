7f71a6e9f2318588e3a2bb21b5c4326a446c7e5e
DamianMatysko <44482004+DamianMatysko@users.noreply.github.com>
2020-04-24 11:18:00 +0200

    add tests

---
+++ src/Tests/binarytree/NodeTest.java
@@ -0,0 +1,44 @@
+package binarytree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void getKey() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void getLeft() {
+    }
+
+    @Test
+    void setLeft() {
+    }
+
+    @Test
+    void getRight() {
+    }
+
+    @Test
+    void setRight() {
+    }
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void isLeaf() {
+    }
+}
---
+++ src/Tests/binarytree/TreeTest.java
@@ -0,0 +1,52 @@
+package binarytree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeTest {
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void printInOrder() {
+    }
+
+    @Test
+    void printPreOrder() {
+    }
+
+    @Test
+    void printPostOrder() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getListOfLeafs() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getParrent() {
+    }
+
+    @Test
+    void getMinRightNode() {
+    }
+}
